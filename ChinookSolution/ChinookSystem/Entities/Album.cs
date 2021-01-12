762126578fa3ed5711e1f5e1dd8e2f908ecf9a43
zozkaa <brzozkaa@hotmail.com>
2021-01-12 14:02:06 -0700

    Created Album entity

--- ChinookSolution/ChinookSystem/Entities/Album.cs
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -11,7 +11,32 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ChinookSystem.Entities
 {
-    class Album
+    [Table("Albums")]
+    internal class Album
     {
+        private string _ReleaseLabel;
+
+        [Key]
+        public int AlbumId { get; set; }
+        
+        [Required(ErrorMessage ="Album Title is required")]
+        [StringLength(160, ErrorMessage = "Album Title is limited to 160 characters.")]
+        public string Title { get; set; }
+                
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        
+        [StringLength(50, ErrorMessage = "Album release label is limited to 50 characters.")]
+        public string ReleaseLabel
+        {
+            get
+            {
+                return _ReleaseLabel;
+            }
+            set
+            {
+                ReleaseLabel = string.IsNullOrEmpty(value) ? null : value;
+            }
+        } // this is a fully implemented property
     }
 }
