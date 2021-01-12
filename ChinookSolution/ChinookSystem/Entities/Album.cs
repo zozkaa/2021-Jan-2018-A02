e9245c40c78cb9ac4a9221bb0d0d18ed380f749a
zozkaa <brzozkaa@hotmail.com>
2021-01-12 13:49:03 -0700

    Created Artist entity

--- ChinookSolution/ChinookSystem/Entities/Album.cs
+++ ChinookSolution/ChinookSystem/Entities/Album.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
 namespace ChinookSystem.Entities
 {
-    class dummy
+    class Album
     {
     }
 }
---
+++ ChinookSolution/ChinookSystem/Entities/Artist.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace ChinookSystem.Entities
+{
+    [Table("Artists")]
+    // Can only be seen in the library class
+    internal class Artist
+    {
+        private string _Name;
+
+        
+        [Key] //primary key with identity specification = yes
+        public int ArtistId { get; set; }
+        // annotations to do validations
+        [StringLength(120, ErrorMessage = "Artist name is limited to 120 characters.")]
+        public string Name {
+            get { 
+                return _Name; 
+            }
+            set {
+                _Name = string.IsNullOrEmpty(value) ? null : value;
+            }
+        } // this is a fully implemented property
+    }
+}
