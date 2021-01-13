cd4926f26244c81a84485dc5451d5d9a4e873487
zozkaa <brzozkaa@hotmail.com>
2021-01-13 13:57:30 -0700

    Set up DAL and connection string

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookSystemContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using ChinookSystem.Entities;
+using System.Data.Entity;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    internal class ChinookSystemContext:DbContext
+    {
+        public ChinookSystemContext() : base("name=ChinookDB")
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<MediaType> MediaTypes { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
