7ad92c25d2b54dbf78a3a5d9eefcbc6a33cde66f
zozkaa <brzozkaa@hotmail.com>
2021-01-13 14:16:57 -0700

    Create BLL class AlbumController, method getArtistAlbums, create viewModel class ArtistAlbums

---
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using ChinookSystem.DAL;
+using ChinookSystem.Entities; // for SQL and are internal
+using ChinookSystem.ViewModels; // for data classes to transfer data from BLL to webapp
+using System.ComponentModel; // for ODS wizard
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController //interface to the outside world
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbums> Albums_GetArtistAlbums()
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name
+                                                    };
+                return results.ToList();
+            }
+        } 
+    }
+}
---
+++ ChinookSolution/ChinookSystem/ViewModels/ArtistAlbums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookSystem.ViewModels
+{
+    public class ArtistAlbums
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ArtistName { get; set; }
+    }
+}
