c00fb0481652db639dc2bb80caacd4004179db46
zozkaa <brzozkaa@hotmail.com>
2021-01-15 15:27:33 -0700

    Filter search album controller method. Get AlbumsForArtist

--- ChinookSolution/ChinookSystem/BLL/AlbumController.cs
+++ ChinookSolution/ChinookSystem/BLL/AlbumController.cs
@@ -30,6 +30,24 @@ namespace ChinookSystem.BLL
                                                     };
                 return results.ToList();
             }
-        } 
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<ArtistAlbums> Albums_GetAlbumsForArtist(int artistid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                IEnumerable<ArtistAlbums> results = from x in context.Albums
+                                                    where x.ArtistId == artistid
+                                                    select new ArtistAlbums
+                                                    {
+                                                        Title = x.Title,
+                                                        ReleaseYear = x.ReleaseYear,
+                                                        ArtistName = x.Artist.Name,
+                                                        ArtistId = x.ArtistId
+                                                    };
+                return results.ToList();
+            }
+        }
     }
 }
