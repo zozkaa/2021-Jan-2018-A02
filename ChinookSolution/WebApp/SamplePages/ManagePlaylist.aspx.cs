9eb59c3cd10ffe4e103859b2180ab7cc6306f99d
zozkaa <brzozkaa@hotmail.com>
2021-02-24 14:42:06 -0700

    Added OLTP BLL, Viewmodel, Sample Page

---
+++ ChinookSolution/WebApp/SamplePages/ManagePlaylist.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+#region Additonal Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.ViewModels;
+
+#endregion
+
+namespace WebApp.SamplePages
+{
+    public partial class ManagePlaylist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TracksSelectionList.DataSource = null;
+        }
+
+        #region Error Handling
+        protected void SelectCheckForException(object sender,
+               ObjectDataSourceStatusEventArgs e)
+        {
+            MessageUserControl.HandleDataBoundException(e);
+        }
+        protected void InsertCheckForException(object sender,
+              ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been added");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        protected void UpdateCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been updated");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        protected void DeleteCheckForException(object sender,
+             ObjectDataSourceStatusEventArgs e)
+        {
+            if (e.Exception == null)
+            {
+                MessageUserControl.ShowInfo("Process Success", "Album has been removed");
+            }
+            else
+            {
+                MessageUserControl.HandleDataBoundException(e);
+            }
+
+        }
+        #endregion
+
+
+        protected void ArtistFetch_Click(object sender, EventArgs e)
+        {
+            
+                //code to go here
+
+          }
+
+
+        protected void GenreFetch_Click(object sender, EventArgs e)
+        {
+
+                //code to go here
+
+        }
+
+        protected void AlbumFetch_Click(object sender, EventArgs e)
+        {
+
+                //code to go here
+
+        }
+
+        protected void PlayListFetch_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveDown_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveUp_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void MoveTrack(int trackid, int tracknumber, string direction)
+        {
+            //call BLL to move track
+ 
+        }
+
+
+        protected void DeleteTrack_Click(object sender, EventArgs e)
+        {
+            //code to go here
+ 
+        }
+
+        protected void TracksSelectionList_ItemCommand(object sender, 
+            ListViewCommandEventArgs e)
+        {
+            //code to go here
+            
+        }
+
+    }
+}
